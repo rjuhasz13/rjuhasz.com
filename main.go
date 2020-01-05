96f53f5b6669125a09387b059c226c04c4dcc8d2
Gabor Szakacs <gabor@simp.hu>
2020-01-05 12:27:41 +0100

    Add home template and styling

--- main.go
+++ main.go
@@ -11,10 +11,11 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type ResearchPageData struct {
-	PageTitle     string  `yaml:"page_title"`
+type Data struct {
+	CV            string  `yaml:"cv"`
 	Publications  []Paper `yaml:"publications"`
 	WorkingPapers []Paper `yaml:"working_papers"`
+	Slug          string
 }
 
 type Paper struct {
@@ -50,34 +51,61 @@ type Medium struct {
 }
 
 func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/research", researchHandler)
+	http.ListenAndServe("localhost:"+os.Getenv("PORT"), nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	tmpl := template.Must(template.ParseFiles("html/base.tmpl", "html/home.tmpl", "html/style.tmpl"))
+
+	data := data()
+	data.Slug = ""
+	err := tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func researchHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/research" {
+		http.NotFound(w, r)
+		return
+	}
+
+	data := data()
+	data.Slug = "research"
+	tmpl, err := template.New("base.tmpl").Funcs(template.FuncMap{
+		"plus1": func(x int) int {
+			return x + 1
+		},
+	}).ParseFiles("html/base.tmpl", "html/research.tmpl", "html/style.tmpl")
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func data() Data {
 	yamlFile, err := ioutil.ReadFile("data/research.yml")
 	if err != nil {
 		panic(err)
 	}
 
-	var data ResearchPageData
+	var data Data
 	err = yaml.Unmarshal(yamlFile, &data)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(data)
-	tmpl, err := template.New("research.tmpl").Funcs(template.FuncMap{
-		"plus1": func(x int) int {
-			return x + 1
-		},
-	}).ParseFiles("html/research.tmpl")
-	if err != nil {
-		panic(err)
-	}
-
-	err = tmpl.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, data)
-	})
-	http.ListenAndServe(":3000", nil)
+	return data
 }
